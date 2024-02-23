fb2262c91c1d698de3ad79ded0e50174bd232f3b
John Davies <jd.other@hotmail.com>
2024-02-23 22:11:25 +1300

    added gRPC boilerplate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,47 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
-const port = 3000
+const port = 3001
+
+// gRPC imports
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const fs = require("fs");
+
+process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
+
+const loaderOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+};
+
+const packageDefinition = protoLoader.loadSync('lightning.proto', loaderOptions);
+
+// Load lnd macaroon
+let m = fs.readFileSync('<PATH TO MACAROON>');
+let macaroon = m.toString('hex');
+
+// Build meta data credentials
+let metadata = new grpc.Metadata()
+metadata.add('macaroon', macaroon)
+let macaroonCreds = grpc.credentials.createFromMetadataGenerator((_args, callback) => {
+  callback(null, metadata);
+});
+
+// Combine credentials
+let lndCert = fs.readFileSync('<PATH TO TLS CERT>');
+let sslCreds = grpc.credentials.createSsl(lndCert);
+let credentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds);
+
+// Create client
+let lnrpcDescriptor = grpc.loadPackageDefinition(packageDefinition);
+let lnrpc = lnrpcDescriptor.lnrpc;
+let client = new lnrpc.Lightning('<HOST>:<PORT>', credentials);
 
 app.get("/", (req: Request, res: Response) => {
-  console.log('Received!')
+  console.log(`${req.method} ${req.path} `)
   res.send("Express + TypeScript Server");
 });
 
