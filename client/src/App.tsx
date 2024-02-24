ae47f9c2e33c15ac3ccc2ece675b04f1df330498
John Davies <jd.other@hotmail.com>
2024-02-24 21:50:59 +1300

    basic frontend connect form UI implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,17 @@
-import React from "react";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex justify-center items-center">
+      <form className="lg:w-[50%] w-[80%] h-[80%] border border-black rounded-lg flex flex-col px-[5%] pt-[5%]">
+        <label>Host</label>
+        <input className="border border-neutral-300 rounded-lg mb-[30px]"></input>
+        <label>TLS Certificate</label>
+        <input className="h-[200px] border border-neutral-300 rounded-lg mb-[30px]"></input>
+        <label>Macaroon</label>
+        <input className="border border-neutral-300 rounded-lg mb-[30px]"></input>
+        <button className="bg-orange-500 py-[5px] px-[20px] rounded-lg text-white ml-auto w-fit">
+          Connect
+        </button>
+      </form>
     </div>
   );
 }
